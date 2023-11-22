d3276f4241fadff054c72d81469b03b96b7749d3
rasikumar <rasimax55@gmail.com>
2023-11-22 15:06:07 +0530

    fetching data using api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,28 @@
+import Results from '@/components/Results';
 import Image from 'next/image'
 
-export default function Home() {
-    return (
-       <div>
-        <h1 className='text-red-400'></h1>
-       </div> 
-    )
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+if (!res.ok ) {
+    throw new Error("failed to fetch a data");
+}
+const data = await res.json();
+
+const results = data.results;
+
+return (
+    <div>
+        <Results results={results} />
+    </div>
+)
 }
